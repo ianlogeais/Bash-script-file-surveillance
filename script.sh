e3dd73dd596fdf903aae45c158270bead58e0076
ianlogeais <71567996+ianlogeais@users.noreply.github.com>
2021-10-30 23:04:33 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+#!/bin/bash
+
+modif1(){
+
+while sleep 1 
+        do 
+               md5v1=$(md5sum "$1" | cut -d' ' -f1) 
+               md5v2=$(md5sum "$2" | cut -d' ' -f1)
+
+                if [ "$md5v2" != "$md5v1" ]
+                then 
+                        echo "Les fichiers sont différents" 
+                        break 
+                fi
+        done
+}
+
+modif2(){ 
+
+while sleep 1 
+        do 
+               md5v1=$(md5sum "$1" | cut -d' ' -f1) 
+               md5v2=$(md5sum "$2" | cut -d' ' -f1)
+
+                if [ "$md5v2" == "$md5v1" ]
+                then 
+                        echo "Les fichiers sont indentiques" 
+                        break 
+                fi
+        done
+}
+
+while true
+do 
+   modif1 "$1" "$2"
+   modif2 "$1" "$2"
+done
